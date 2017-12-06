9b7dd33a0500fbc4775ccd6662139652836b4807
goost <improbity39@gmail.com>
2017-12-06 12:09:39 +0800

    hi

---
+++ src/main/java/improbity39/show.java
@@ -0,0 +1,9 @@
+package improbity39;
+public class show {
+    public static String re(int a){
+        if(a==1)
+            return "Ya";
+        else
+            return "No";
+    }
+}
---
+++ src/test/java/improbity39/showTest.java
@@ -0,0 +1,15 @@
+package improbity39;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class showTest {
+    @Test
+    public void re() throws Exception {
+
+        String ans = show.re(2);
+        System.out.println(ans);
+    }
+
+}
